ce990b82bab5096431b0605f83dc3adc871b54ab
Benjamin Daniel Morgan <benjamindanielmorgan@benjamins-mbp.home>
2020-11-24 17:26:04 +0000

    first commit

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ContosoUniversity.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+
+    public class Course
+    {
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+        
+    }
+
+   
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ContosoUniversity.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+
+    public class Student
+    {
+		public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+   
+}
